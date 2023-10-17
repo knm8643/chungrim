2b398c164cab90dd642fb84174037d734c1c125e
이충재 <knm8643@nate.com>
2023-10-17 17:41:51 +0900

    java 폴더 추가

---
+++ src/main/java/com/tsib/chungrim/ChungrimApplication.java
@@ -0,0 +1,16 @@
+package com.tsib.chungrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class ChungrimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChungrimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsib/chungrim/ChungrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsib.chungrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChungrimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
