9c685a253630e30d68bf9f449b91ec7f57a3b83b
이충재 <knm8643@nate.com>
2023-10-24 12:00:50 +0900

    index 호출경로 설정

--- src/main/java/com/tsib/chungrim/ChungrimApplication.java
+++ src/main/java/com/tsib/chungrim/ChungrimApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-@SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ChungrimApplication {
 
     public static void main(String[] args) {
